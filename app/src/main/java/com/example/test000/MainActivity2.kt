7fc5473e9f8cd7bbc88598f11853b50e0f7e1ddb
OmarMagdy2k <74633590+OmarMagdy2k@users.noreply.github.com>
2023-02-25 23:06:39 +0200

    25/2/2023

--- app/src/main/java/com/example/test000/MainActivity2.kt
+++ app/src/main/java/com/example/test000/MainActivity2.kt
@@ -3,9 +3,20 @@ package com.example.test000
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+import android.provider.MediaStore
+import android.view.View
 import android.widget.Button
+import android.widget.MediaController
+import android.widget.VideoView
+
 
 class MainActivity2 : AppCompatActivity() {
+
+private lateinit var videoView: VideoView
+private var ourRequestCode : Int = 123 // any number
+
+    //Change Pages
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
@@ -14,12 +25,38 @@ class MainActivity2 : AppCompatActivity() {
         button1.setOnClickListener {
             val intent = Intent(this, MainActivity3::class.java)
             startActivity(intent)
+
         }
 
         val button2 = findViewById<Button>(R.id.home_button)
         button2.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+        }
+        videoView = findViewById<VideoView>(R.id.record_videoView)
+        // now set up media controller for the play pause next pre
+
+        val mediaCollection = MediaController(this)
+        mediaCollection.setAnchorView(videoView)
+        videoView.setMediaController(mediaCollection)
+
+    }
+
+    fun startVideo(view: View) {
+        //start intent to capture video
+        val intent = Intent(MediaStore.ACTION_VIDEO_CAPTURE)
+        if(intent.resolveActivity(packageManager)!=null){
+            startActivityForResult(intent,ourRequestCode)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == ourRequestCode && resultCode == RESULT_OK){
+            // get data from uri
+            val videoUri = data?.data
+            videoView.setVideoURI(videoUri)
+            videoView.start()
         }
     }
 }
