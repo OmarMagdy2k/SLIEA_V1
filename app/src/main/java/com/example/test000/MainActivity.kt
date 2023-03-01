1c8060041c043bcd782afeed7146fae842aad273
OmarMagdy2k <74633590+OmarMagdy2k@users.noreply.github.com>
2023-03-01 08:54:33 +0200

    1/3/2023 ((Validation for home page)) "navigate pages"

--- app/src/main/java/com/example/test000/MainActivity.kt
+++ app/src/main/java/com/example/test000/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.test000
 
 import android.os.Bundle
 import android.content.Intent
+import android.view.View
 import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -10,11 +14,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.confirm_button1)
+        val confirmButton = findViewById<Button>(R.id.confirm_button1)
+        val processGroup = findViewById<RadioGroup>(R.id.processGroup)
 
-        button.setOnClickListener {
-            val intent = Intent(this,MainActivity2::class.java)
-            startActivity(intent)
+        processGroup.setOnCheckedChangeListener { group, checkedId ->
+            if(checkedId == R.id.vt2slButton){
+                confirmButton.setOnClickListener {
+                    val intent = Intent(this,MainActivity3::class.java)
+                    startActivity(intent)
+                }
+            }
+            else if (checkedId == R.id.sl2vtButton){
+                confirmButton.setOnClickListener {
+                    val intent = Intent(this,MainActivity2::class.java)
+                    startActivity(intent)
+                }
+            }
         }
     }
 }
