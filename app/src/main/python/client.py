9576736b8105a931863277e337587c05bf467ef0
OmarMagdy2k <74633590+OmarMagdy2k@users.noreply.github.com>
2023-05-03 01:57:19 +0300

    Enhancement

--- app/src/main/python/client.py
+++ app/src/main/python/client.py
@@ -5,12 +5,12 @@ import requests
 def translate_image_En(file_name):
     s = requests.Session()
 
-    r = s.get("http://192.168.1.5:8000/translate_image/", params={'file_name': file_name})
+    r = s.get("http://192.168.1.5:8000/translate_image_En/", params={'file_name': file_name})
     return r.json()['prediction']
 
 def translate_image_Ar(file_name):
     s = requests.Session()
-    r = s.get("http://192.168.1.5:8000/translate_image/", params={'file_name': file_name})
+    r = s.get("http://192.168.1.5:8000/translate_image_Ar/", params={'file_name': file_name})
     return r.json()['prediction']
 
 # print(translate_image('image2621101635021160227.jpg'))
