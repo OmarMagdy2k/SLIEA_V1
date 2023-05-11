e4e28c727381f481b46f420bd5ffd1ae2a09cf75
OmarMagdy2k <74633590+OmarMagdy2k@users.noreply.github.com>
2023-05-11 20:03:44 +0300

    Enhancement:

--- app/src/main/python/client.py
+++ app/src/main/python/client.py
@@ -5,17 +5,17 @@ import requests
 def translate_image_En(file_name):
     s = requests.Session()
 
-    r = s.get("http://192.168.1.5:8000/translate_image_En/", params={'file_name': file_name})
+    r = s.get("http://192.168.1.5:5000/translate_image_En/", params={'file_name': file_name})
     return r.json()['prediction']
 
 def translate_image_Ar(file_name):
     s = requests.Session()
-    r = s.get("http://192.168.1.5:8000/translate_image_Ar/", params={'file_name': file_name})
+    r = s.get("http://192.168.1.5:5000/translate_image_Ar/", params={'file_name': file_name})
     return r.json()['prediction']
 
 def autocorrect_En(statement):
     s = requests.Session()
-    r = s.get("http://192.168.1.5:8000/autocorrect_En/", params={'statement': statement})
+    r = s.get("http://192.168.1.5:5000/autocorrect_En/", params={'statement': statement})
     return r.json()['corrected_statement']
 
 
